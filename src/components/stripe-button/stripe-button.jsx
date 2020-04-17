8d4a99e1fae60f20ce1607b6fb4c8eb143fcbab7
Nikesh <nikeshdevkota28@gmail.com>
2020-04-17 20:46:44 +0545

    fourth commit

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_R9rf4F2E7NUtdB21Q95shM4700AnnsA5bs';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='E-commerce'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeCheckoutButton;
